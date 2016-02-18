ce70bbd110cc5fadf33095ff42ac189b381320a1
ctevnan <carolyntevnan@yahoo.com>
2016-02-18 12:28:18 -0500

    adding to server.js. Accidentally deleted 2 folders in node_modules, concat-map, and iconv-lite. will need to add those back

--- server.js
+++ server.js
@@ -2,17 +2,39 @@ var express = require('express');
 var mysql = require('mysql');
 var expressHandlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('rcb_authentication_db', 'root');
 
-var connection = mysql.createConnection({
-   port: 3000,
-   host: 'localhost',
-   user: 'root',
-  database: 'rcb_authentication_db'
+var User = sequelize.define('user', {
+      email: {
+             type: Sequelize.STRING,
+             unique: true
+      },
+    password: Sequelize.STRING,
+    firstname: Sequelize.STRING,
+    lastname: Sequelize.STRING,  
 });
 
 var PORT = process.env.NODE_ENV || 3000;
 
 var app = express();
+
+var connection = mysql.createConnection({
+   port: 3306,
+   host: 'localhost',
+   user: 'root',
+  database: 'rcb_authentication_db'
+});
+
+app.use(session({
+        secret: "this is a secret",
+        cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 14
+        },
+        saveUninitialized: true,
+        resave: false
+}));        
 
 app.engine('handlebars', expressHandlebars({
    defaultLayout: 'main'
