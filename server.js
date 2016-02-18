ad621d0662c43af12a6416868bfe85494f1f9e79
ctevnan <carolyntevnan@yahoo.com>
2016-02-18 12:55:52 -0500

    finished up server.js. still need to add concat-map and iconv-lite back into node_modules

--- server.js
+++ server.js
@@ -43,7 +43,16 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({
    extended: false
-})))
+}));
+
+
+app.get('/', function(req, res) {
+        res.render('index', {
+          msg: req.query.msg
+        });
+});
+
+
 
 connection.connect(function(err) {
 if (err) {
@@ -58,29 +67,48 @@ app.get ('/', function(req,res) {
 var email + req.body.email;
 var password + req.body.password;
 
-connection.query("SELECT * FROM users WHERE email=?", [email], function(err,results) {
-  if (err) {
-    throw err;
-  }
-
-  if (results.length === 0) {
-    //create an account
-    connection.query("INSERT INTO users (email, password) VALUES (?, ?)", [email, password], function(err) {
-     if (err) {
-      throw err;
-      }
-    
-    res.redirect('/success');
-    });
-  } else {
-    res.redirect('/?msg=Email');
-  } 
+app.post('/register', function(req, res) {
+  User.create(req.body).then(function(user) {
+        req.session.authenticated = user;
+        res.redirect('/secret');
+  }).catch(function(err) {
+                    res.redirect("/?msg=" + err.message);
+  });
 });
 
 app.post('/login', function(req, res) {
   var email = req.body.email;
-  var password = req.body.passsword;
+  var password = req.body.password;
 
- connection.query
-}   
-  
+  User.findOne({
+          where: {
+                   email: email,
+                   password: password
+          }
+  }).then(function(user) {
+          if(user) {
+                   req.session.authenticated = user;
+                   res.redirect('/secret');
+          } else {
+                   res.redirect('/?msg=Invalid login');
+          }
+  }).catch(function(err) {
+          throw err;
+  });
+});
+
+app.get('/secret', funtion(req,res) {
+
+      //if user is authenticated
+      if(req.session.authenticated) {
+             res.render("secret");
+      } else {
+             res.redirect("/msg=you are not logged in");
+      }
+});
+
+sequelize.sync().then(function() {
+        app.listen(PORT, function() {
+                console.log("Listening on port %s", PORT);
+        })
+});
